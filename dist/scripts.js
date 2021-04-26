efff9c16900297a4b3fffab63ffb32300ff41634
Chaggai Thal <78046067+Chaggai@users.noreply.github.com>
2021-04-26 11:47:27 +0300

    Update scripts.js

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,29 +1,43 @@
-import {NUM_OF_QUESTIONS} from "./constants.js";
 const $loader = document.querySelector(".loader");
 const $startBtn = document.querySelector(".start");
 const $questionWrap = document.querySelector(".questionWrap");
 const $question = document.querySelector("#question");
 const $answerWrap = document.querySelector(".answersWrap");
 const $answers = document.querySelectorAll(".answer");
+const $correctAnswer = document.querySelector("#correctAnswer");
 const $title = document.querySelector(".title");
+const $startForm = document.querySelector(".start-form");
+const $numOfQuestions = document.querySelector("#trivia_amount");
+const $category = document.querySelector('[name="trivia_category"]');
+const $difficulty = document.querySelector('[name="trivia_difficulty"]');
 let questions = [];
 let questionIndex;
 let score = 0;
-const initGame = () => {
-  questionIndex = 1;
-  showHideQuestion("show");
-  getQuestionsFromApi().then((response) => {
-    $loader.style.display = "none";
-    questions = response;
-    setQuestion();
-  });
+let numOfQuestions;
+const initGame = (e) => {
+  e.preventDefault();
+  numOfQuestions = +$numOfQuestions.value;
+  const categoryOption = +$category.value > 0 ? `&category=${$category.value}` : "";
+  const difficultyOption = $difficulty.value !== "any" ? `&difficulty=${$difficulty.value}` : "";
+  const url = `https://opentdb.com/api.php?amount=${numOfQuestions}${categoryOption}${difficultyOption}`;
+  if (+$numOfQuestions.value > 0 && +$numOfQuestions.value <= 50) {
+    questionIndex = 1;
+    score = 0;
+    showHideQuestion("show");
+    getQuestionsFromApi(url).then((response) => {
+      $loader.style.display = "none";
+      questions = response;
+      setQuestion();
+    });
+  }
 };
-const getQuestionsFromApi = async () => {
-  const response = await fetch(`https://opentdb.com/api.php?amount=${NUM_OF_QUESTIONS}`);
+const getQuestionsFromApi = async (url) => {
+  const response = await fetch(url);
   const data = await response.json().then((data2) => data2.results);
   return data;
 };
 const showHideQuestion = (action) => {
+  $startForm.style.display = action === "show" ? "none" : "block";
   $loader.style.display = action === "show" ? "flex" : "none";
   $startBtn.style.display = action === "show" ? "none" : "block";
   $questionWrap.style.display = action === "show" ? "block" : "none";
@@ -32,8 +46,9 @@ const showHideQuestion = (action) => {
 const setQuestion = () => {
   const {question, correct_answer, incorrect_answers} = questions[questionIndex - 1];
   const answers = [...incorrect_answers, correct_answer];
-  $title.innerHTML = `Question ${questionIndex} of 10`;
+  $title.innerHTML = `Question ${questionIndex} of ${numOfQuestions}`;
   $question.innerHTML = question;
+  $correctAnswer.innerHTML = correct_answer;
   answers.sort(() => 0.5 - Math.random());
   answers.forEach((answer, index) => {
     $answers[index].style.display = "block";
@@ -51,7 +66,7 @@ const checkAnswer = ({target}) => {
     score++;
   } else {
     $answers.forEach(($answer) => {
-      if ($answer.innerHTML === correct_answer) {
+      if ($answer.innerHTML === $correctAnswer.innerHTML) {
         $answer.classList.add("correct");
       }
     });
@@ -61,7 +76,7 @@ const checkAnswer = ({target}) => {
   setTimeout(() => {
     target.classList.remove("incorrect");
     $answers.forEach(($answer) => $answer.classList.remove("correct"));
-    if (questionIndex <= NUM_OF_QUESTIONS) {
+    if (questionIndex <= numOfQuestions) {
       $answers.forEach(($answer) => $answer.style.display = "none");
       setQuestion();
     } else {
